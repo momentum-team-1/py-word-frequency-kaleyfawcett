aa919b2fc7e75d51a758dd748d47c6be7936f685
kaleyfawcett <kaley.fawcett@gmail.com>
2020-05-20 19:01:46 -0400

    All steps completed

--- word_frequency.py
+++ word_frequency.py
@@ -7,7 +7,7 @@ STOP_WORDS = [
 
 def clean_text(text):
     text = text.lower()
-    all_letters = "abcdefghijklmnopqrstuvwxyz "
+    all_letters = "abcdefghijklmnopqrstuvwxyz -"
     text_to_keep = ""
     for char in text:
         if char in all_letters:
@@ -19,21 +19,37 @@ def print_word_freq(file):
     
     file = open ("seneca_falls.txt")
     text = file.read()
-    cleaner_text = clean_text(text)
+    line_break = text.replace ("\n", " ")
+    replaced_space = line_break.replace ("  ", " ")
+    remove_double_dash = replaced_space. replace ("--", " ") 
+    cleaner_text = clean_text(remove_double_dash) 
     split_text = cleaner_text.split(" ")
     word_scramble = []
     for word in split_text:
         if not word in STOP_WORDS:
             word_scramble.append(word)
+    sort_words = sorted (word_scramble) 
     
+       
+    multiple_words = dict()
+
+    for word in sort_words:
+        if word in multiple_words:
+            multiple_words[word] += 1
+        else:
+            multiple_words[word] = 1
+    multiple_words = {key: value for key, value in multiple_words.items() if value >=1} 
+
+    sorted_words = sorted(multiple_words.items(), key=lambda seq: seq[1], reverse=True) 
     
-    print (word_scramble) 
+
+    for key, value in sorted_words:
+            print (key.rjust(16) , ' | ', value, value * ("*")) 
+
 
 
 
     
-
-
 
 
 
